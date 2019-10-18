0b80df3df1d61412a58f4666ce9f225d4b4e41da
Kroh Nickolay <mikolka.del@gmail.com>
2019-10-19 02:22:12 +0300

    feat: implement Nodde.js

--- src/max-heap.js
+++ src/max-heap.js
@@ -2,15 +2,25 @@ const Node = require('./node');
 
 class MaxHeap {
 	constructor() {
-		
+		this.root = null;
+		this.parentNodes = [];
 	}
 
 	push(data, priority) {
-		
+		let n = new Node(data, priority);
+
+		this.insertNode(n);
+		this.shiftNodeUp(n);
 	}
 
 	pop() {
-		
+		if(this.root !== null) {
+			let root = this.root;
+
+			this.detachRoot();
+
+			return root.data;
+		}
 	}
 
 	detachRoot() {
@@ -30,15 +40,40 @@ class MaxHeap {
 	}
 
 	clear() {
-		
+		this.root = null;
+		this.parentNodes = [];
 	}
 
 	insertNode(node) {
-		
+		if(this.root === null)
+			this.root = node;
+		else {
+			this.parentNodes[0].appendChild(node);
+
+			if(this.parentNodes[0].right !== null)
+				this.parentNodes.shift();
+		}
+	
+		this.parentNodes.push(node);
 	}
 
 	shiftNodeUp(node) {
-		
+		if(node.parent === null) {
+			this.root = node;
+			return;
+		}
+
+		let nodeIndex = this.parentNodes.indexOf(node);
+		if(nodeIndex >= 0) {
+			let parentIndex = this.parentNodes.indexOf(node.parent);
+			if(parentIndex >= 0)
+				this.parentNodes[parentIndex] = node;
+			
+			this.parentNodes[nodeIndex] = node.parent;
+		}
+
+		node.swapWithParent();
+		this.shiftNodeUp(node);
 	}
 
 	shiftNodeDown(node) {
--- src/node.js
+++ src/node.js
@@ -1,22 +1,83 @@
 class Node {
 	constructor(data, priority) {
-
+		this.data = data;
+		this.priority = priority;
+		this.parent = null;
+		this.left = null;
+		this.right = null;
 	}
 
 	appendChild(node) {
-
+		if(this.left === null) {
+			this.left = node;
+			node.parent = this;
+		}
+		else if(this.right === null) {
+			this.right = node;
+			node.parent = this;
+		}
 	}
 
 	removeChild(node) {
-
+		if(this.left === node) {
+			this.left.parent = null
+			this.left = null;
+		}
+		else if(this.right === node) {
+			this.left.parent = null
+			this.right = null;
+		}
+		else throw new new SyntaxError("Node has no child");;
 	}
 
 	remove() {
-
+		if(this.parent !== null)
+			this.parent.removeChild(this);
 	}
 
 	swapWithParent() {
-		
+		if(this.parent !== null) {
+			if(this.parent.parent !== null) {
+				if(this.parent.parent.right === this.parent)
+					this.parent.parent.right = this;
+				else
+					this.parent.parent.left = this;
+			}
+
+			if(this.left !== null)
+				this.left.parent = this.parent;
+			if(this.right !== null)
+				this.right.parent = this.parent;
+
+			if(this.parent.left !== null) {
+				if(this.parent.left !== this) {
+					this.parent.left.parent = this;
+
+					let temp = this.parent.left;
+					this.parent.left = this.left;
+					this.left = temp;
+				} else {
+					this.parent.left = this.left;
+					this.left = this.parent;
+				}
+			}
+			if(this.parent.right !== null) {
+				if(this.parent.right !== this) {
+					this.parent.right.parent = this;
+
+					let temp = this.parent.right;
+					this.parent.right = this.right;
+					this.right = temp;
+				} else {
+					this.parent.right = this.right;
+					this.right = this.parent;
+				}
+			}
+
+			let temp = this.parent.parent;
+			this.parent.parent = this;
+			this.parent = temp;
+		}
 	}
 }
 
