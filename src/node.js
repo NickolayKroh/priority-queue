7a8cd737ae4872f9dad45873675f476b228fc238
Kroh Nickolay <mikolka.del@gmail.com>
2019-10-20 21:17:30 +0300

    fix: wrong pointer in node.removechild

--- src/node.js
+++ src/node.js
@@ -24,60 +24,62 @@ class Node {
 			this.left = null;
 		}
 		else if(this.right === node) {
-			this.left.parent = null
+			this.right.parent = null
 			this.right = null;
 		}
-		else throw new new SyntaxError("Not a child");;
+		else
+			throw new new SyntaxError("Not a child");;
 	}
 
 	remove() {
-		if(this.parent !== null)
+		if(this.parent)
 			this.parent.removeChild(this);
 	}
 
 	swapWithParent() {
-		if(this.parent !== null) {
-			if(this.parent.parent !== null) {
-				if(this.parent.parent.right === this.parent)
-					this.parent.parent.right = this;
-				else
-					this.parent.parent.left = this;
-			}
-
-			if(this.left !== null)
-				this.left.parent = this.parent;
-			if(this.right !== null)
-				this.right.parent = this.parent;
-
-			if(this.parent.left !== null) {
-				if(this.parent.left !== this) {
-					this.parent.left.parent = this;
-
-					let temp = this.parent.left;
-					this.parent.left = this.left;
-					this.left = temp;
-				} else {
-					this.parent.left = this.left;
-					this.left = this.parent;
-				}
-			}
-			if(this.parent.right !== null) {
-				if(this.parent.right !== this) {
-					this.parent.right.parent = this;
-
-					let temp = this.parent.right;
-					this.parent.right = this.right;
-					this.right = temp;
-				} else {
-					this.parent.right = this.right;
-					this.right = this.parent;
-				}
-			}
-
-			let temp = this.parent.parent;
-			this.parent.parent = this;
-			this.parent = temp;
+		if( !this.parent )
+			return;
+		
+		if(this.parent.parent) {
+			if(this.parent.parent.right === this.parent)
+				this.parent.parent.right = this;
+			else
+				this.parent.parent.left = this;
 		}
+
+		if(this.left)
+			this.left.parent = this.parent;
+		if(this.right)
+			this.right.parent = this.parent;
+
+		if(this.parent.left) {
+			if(this.parent.left === this) {
+				this.parent.left = this.left;
+				this.left = this.parent;
+			} else {
+				this.parent.left.parent = this;
+
+				let temp = this.parent.left;
+				this.parent.left = this.left;
+				this.left = temp;
+			}
+		}
+		if(this.parent.right) {
+			if(this.parent.right === this) {
+				this.parent.right = this.right;
+				this.right = this.parent;
+			} else {
+				this.parent.right.parent = this;
+
+				let temp = this.parent.right;
+				this.parent.right = this.right;
+				this.right = temp;
+			}
+		}
+
+		let temp = this.parent.parent;
+		this.parent.parent = this;
+		this.parent = temp;
 	}
 }
 
